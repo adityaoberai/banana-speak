0837aee2abd31687d7c972caba88ec5e51fea1b7
Aditya Oberai <adityaoberai1@gmail.com>
2022-11-05 21:10:40 +0530

    Minor refactoring

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var txtInput = document.querySelector("#txt-input")
 var txtOutput = document.getElementById("txt-output")
 
 function errorHandler(error) {
-    alert("Error Occured", error)
+    alert("Error occured, please try again later")
     txtOutput.innerText = "Error Occured\n\n" + error
+    console.log(error)
 }
 
 function clickHandler() {
-    fetch("https://api.funtranslations.com/translate/minion.json?text="+txtInput.value)
+    fetch("https://api.funtranslations.com/translate/minion.json?text=" + txtInput.value)
     .then(response => response.json())
     .then(json => txtOutput.innerText = json.contents.translated)
     .catch(errorHandler)
